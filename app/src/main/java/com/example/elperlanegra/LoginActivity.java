c25898e739c4b35250a58ad6b91c5d8d7482634d
Geraldine Drouet <gbdrouet@itb.edu.ec>
2023-03-15 12:56:15 -0500

    clicklistener en textview registro en LoginActivity e iniciarsesion en RegistroActivity

--- app/src/main/java/com/example/elperlanegra/LoginActivity.java
+++ app/src/main/java/com/example/elperlanegra/LoginActivity.java
@@ -2,13 +2,43 @@ package com.example.elperlanegra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button login;
+    EditText correoLog, contrasenaLog;
+    TextView registroEnLog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        correoLog = findViewById(R.id.et_correoLog);
+        contrasenaLog = findViewById(R.id.et_contrasenaLog);
+        login = findViewById(R.id.buttonLog);
+        registroEnLog =  findViewById(R.id.tv_regLog);
+
+        //ClickListener para el textview que lleva a RegistroActivity
+        registroEnLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegistroActivity.class));
+            }
+        });
+
+        //ClickListener para el botón que inicia sesión
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/elperlanegra/RegistroActivity.java
+++ app/src/main/java/com/example/elperlanegra/RegistroActivity.java
@@ -2,13 +2,46 @@ package com.example.elperlanegra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegistroActivity extends AppCompatActivity {
+
+    Button registro;
+    EditText nombreApellido, direccion, telefono, correo, contrasena;
+    TextView loginEnRegistro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        nombreApellido = findViewById(R.id.et_nombreAp);
+        direccion = findViewById(R.id.et_direccion);
+        telefono = findViewById(R.id.et_telefono);
+        correo = findViewById(R.id.et_correo);
+        contrasena = findViewById(R.id.et_contrasena);
+        registro = findViewById(R.id.buttonReg);
+        loginEnRegistro = findViewById(R.id.tv_logReg);
+
+        //ClickListener para el textview que lleva a LoginActivity
+        loginEnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegistroActivity.this, LoginActivity.class));
+            }
+        });
+
+        //ClickListener para el botón que registra
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
