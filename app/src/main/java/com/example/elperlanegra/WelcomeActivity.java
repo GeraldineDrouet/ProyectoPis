1f8ed60df4864ec56a6d856cf766c645ef4c1491
Geraldine Drouet <gbdrouet@itb.edu.ec>
2023-03-15 13:37:54 -0500

    onclick en WelcomeActivity en boton registrarse y textview iniciar sesion

--- app/src/main/java/com/example/elperlanegra/WelcomeActivity.java
+++ app/src/main/java/com/example/elperlanegra/WelcomeActivity.java
@@ -2,7 +2,9 @@ package com.example.elperlanegra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void registrarse(View view) {
+        startActivity(new Intent(WelcomeActivity.this, RegistroActivity.class));
+    }
+
+    public void loginWelcome(View view) {
+        startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
     }
 }
