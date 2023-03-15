7e0387de72dbe9fdbadcbea71716624b6b0ad7b5
Geraldine Drouet <gbdrouet@itb.edu.ec>
2023-03-15 16:36:53 -0500

    Mantener sesion abierta y boton cerrar sesion

--- app/src/main/java/com/example/elperlanegra/MainActivity.java
+++ app/src/main/java/com/example/elperlanegra/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.elperlanegra;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -15,11 +19,16 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.elperlanegra.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    private FirebaseAuth authUser;
+    private FirebaseUser mUser;
+    Button btn_logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +38,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
+
+        authUser = FirebaseAuth.getInstance();
+        mUser = authUser.getCurrentUser();
+
+        btn_logout = findViewById(R.id.btn_logout);
+        //CLICK LISTENER PARA MÉTODO CERRAR SESIÓN
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                authUser.signOut();
+            }
+        });
+
+        //CUANDO EL USUARIO CIERRA SESIÓN VUELVE A WELCOME ACTIVITY
+        authUser.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() == null){
+                    Toast.makeText(MainActivity.this, "¡SESIÓN CERRADA!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
+                    finish();
+                }
+            }
+        });
+
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/elperlanegra/WelcomeActivity.java
+++ app/src/main/java/com/example/elperlanegra/WelcomeActivity.java
@@ -2,16 +2,35 @@ package com.example.elperlanegra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    ProgressBar pb_welcome;
+    FirebaseAuth auth;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        auth =  FirebaseAuth.getInstance();
+        pb_welcome = findViewById(R.id.pb_wel);
+        pb_welcome.setVisibility(View.GONE);
+
+        if (auth.getCurrentUser() != null){
+            pb_welcome.setVisibility(View.VISIBLE);
+            startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+            Toast.makeText(this, "¡SESIÓN INICIADA!", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void registrarse(View view) {
