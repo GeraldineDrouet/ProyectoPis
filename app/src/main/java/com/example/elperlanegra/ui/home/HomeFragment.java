02c6de702ef9cbdfb6bceb37a3e538a7305fc9ba
Geraldine Drouet <gbdrouet@itb.edu.ec>
2023-03-17 16:33:46 -0500

    Recyclerview de productos populares listo

--- app/src/main/java/com/example/elperlanegra/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/elperlanegra/ui/home/HomeFragment.java
@@ -1,26 +1,73 @@
 package com.example.elperlanegra.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.elperlanegra.R;
+import com.example.elperlanegra.adaptadores.PopularAdapter;
 import com.example.elperlanegra.databinding.FragmentHomeBinding;
+import com.example.elperlanegra.modelos.PopularModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    //POPULAR ITEMS
+    //RECYCLERVIEW
+    RecyclerView popularRec;
 
+    //FIREBASE
+    FirebaseFirestore db;
+
+    //POPULAR ITEMS
+    List<PopularModel> popularModelList;
+    PopularAdapter popularAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        db = FirebaseFirestore.getInstance();
+        popularRec = root.findViewById(R.id.pop_rec);
+
+        //Popular items
+        popularRec.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
+        popularModelList = new ArrayList<>();
+        popularAdapter = new PopularAdapter(getActivity(), popularModelList);
+        popularRec.setAdapter(popularAdapter);
+
+        db.collection("ProductosPopulares")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                PopularModel popularModel = document.toObject(PopularModel.class);
+                                popularModelList.add(popularModel);
+                                popularAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(), "Error: " + task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
         return root;
     }
 
