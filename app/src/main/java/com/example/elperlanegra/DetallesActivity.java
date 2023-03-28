1eef0bb45242bcffb9408a3f1e376ff84b080c78
Geraldine Drouet <gbdrouet@itb.edu.ec>
2023-03-27 19:52:21 -0500

    modificaciones menores en DetallesActivity y descripsciones largas en firebase

--- app/src/main/java/com/example/elperlanegra/DetallesActivity.java
+++ app/src/main/java/com/example/elperlanegra/DetallesActivity.java
@@ -9,6 +9,7 @@ import androidx.core.app.NavUtils;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -21,8 +22,11 @@ import java.util.Objects;
 public class DetallesActivity extends AppCompatActivity {
 
     ImageView detailedImg, addItem, removeItem;
-    TextView precio, descripcion, rating;
+    TextView nombre, precio, descripcion, rating, cantidad;
     Button addTocart;
+
+    int cantTotal = 1;
+    double totalPrice = 0;
 
     Toolbar toolbarDetalles;
     VerTodoModel verTodoModel = null;
@@ -46,21 +50,47 @@ public class DetallesActivity extends AppCompatActivity {
         }
 
         detailedImg = findViewById(R.id.detallesImg);
+
         addItem = findViewById(R.id.add_item);
         removeItem = findViewById(R.id.quitar_item);
+        cantidad = findViewById(R.id.number_item);
 
         precio = findViewById(R.id.precio_detalles);
         descripcion = findViewById(R.id.descrip_detalles);
         rating = findViewById(R.id.rating_detalles);
+        nombre = findViewById(R.id.nombre_detalle);
 
         if (verTodoModel != null){
             Glide.with(getApplicationContext()).load(verTodoModel.getImg_url()).into(detailedImg);
+            nombre.setText(verTodoModel.getNombre());
             rating.setText(verTodoModel.getRating());
             descripcion.setText(verTodoModel.getDescripcion());
             precio.setText("Precio: $"+ (double) verTodoModel.getPrecio() + "");
+
+            totalPrice = verTodoModel.getPrecio() * cantTotal;
         }
 
         addTocart = findViewById(R.id.addtocart_bt);
+
+        addItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cantTotal < 10){
+                    cantTotal++;
+                    cantidad.setText(String.valueOf(cantTotal));
+                }
+            }
+        });
+
+        removeItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cantTotal > 1){
+                    cantTotal--;
+                    cantidad.setText(String.valueOf(cantTotal));
+                }
+            }
+        });
     }
 
     @Override
